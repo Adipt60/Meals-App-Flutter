8e1149a472c069c5007c8d505beb6bfd29b41cf4
Aditya Patil <adipt1660@gmail.com>
2023-05-16 10:25:42 +0530

    Added dynamic favourites star icon

--- lib/screens/meal_details.dart
+++ lib/screens/meal_details.dart
@@ -13,6 +13,10 @@ class MealDetailsScreen extends ConsumerWidget {
 
   @override
   Widget build(BuildContext context, WidgetRef ref) {
+    final favouriteMeals = ref.watch(favouriteMealsProvider);
+
+    bool isFavourite = favouriteMeals.contains(meal);
+
     return Scaffold(
       appBar: AppBar(
         title: Text(meal.title),
@@ -31,7 +35,7 @@ class MealDetailsScreen extends ConsumerWidget {
                 ),
               );
             },
-            icon: const Icon(Icons.star),
+            icon: Icon(isFavourite ? Icons.star : Icons.star_border),
           ),
         ],
       ),
